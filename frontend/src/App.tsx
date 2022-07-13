3da6e61acc975c3223761dde7935342514152d40
Luiz Brito <luisshb93@gmail.com>
2022-07-12 21:59:06 -0300

    First component.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NoticationButton from "./components/NotificationButton"
+
 function App() {
   return(
-    <h1>Olá mundo!</h1>
+    <>
+     <h1>Olá mundo!</h1>
+     <NoticationButton/>
+     <NoticationButton/>
+    </>
   )
 }
 
